e5745df277b96966a94a96478f3b25e9b080c29b
rudra290 <rudrapatel3226@gmail.com>
2026-07-11 21:45:30 +0530

    rand version 8.5.

--- guessing_game2/src/main.rs
+++ guessing_game2/src/main.rs
@@ -1,3 +1,17 @@
+use std::io::{self, Write};
+
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guessing Game Part 2");
+    print!("Please enter Any number: ");
+    io::stdout().flush().unwrap();
+
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    let cpu_guess = rand::thread_rng().gen_range(1..=100);
+    println!("CPU Guess {}", cpu_guess);
+    println!("Your Guess {}", guess);
 }
